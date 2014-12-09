052c78211069c9dd35c2ebdafdcd7bf275f959ee
유성매직 <gg6123@naver.com>
2014-12-10 01:36:38 +0900

    예시 프로그램(smi2vtt.py)이 올바른 종료 코드를 반환하도록 수정

--- examples/smi2vtt.py
+++ examples/smi2vtt.py
@@ -4,9 +4,9 @@ from sys import argv, stderr
 from pysami import SmiFile, ConversionError
 
 def throw(code):
-	error = ConversionError(code)
+	error = ConversionError(-1*code)
 	print(error.msg, file=stderr)
-	exit(error.code)
+	exit(code)
 
 smi_filepath = argv[1]
 vtt_filepath = smi_filepath.rsplit('.', 1)[0]+'.vtt'
@@ -16,15 +16,15 @@ try:
 	smi.parse()
 	vtt = smi.convert('vtt', 'KRCC')
 except ConversionError as e:
-	throw(e.code)
+	throw(-1*e.code)
 except:
-	throw(-6)
+	throw(6)
 
 try:
 	vtt_file = open(vtt_filepath, 'w')
 	vtt_file.write(vtt)
 	vtt_file.close()
 except:
-	exit(-5)
+	exit(5)
 
-exit(1)
+exit(0)
